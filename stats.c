ce41e21e306e6e2f71953174b9f9a8bc33a5935b
RobertoDuartee <rduarte.verde@gmail.com>
2022-06-16 00:08:52 -0500

    Adding code outline and documentation for C2M1 Assessment

--- stats.c
+++ stats.c
@@ -27,6 +27,12 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
+int len;
+unsigned char *sorted_array;
+int min;
+int max;
+float median;
+float mean;
 
 int main() {
 
@@ -35,41 +41,105 @@ int main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+
+  len = sizeof(test)/sizeof(test[0]);
+  printf("length of the array: %i\n",len);
+
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+  printf("Original Array: ");
+  print_array(test,len);
+
+  sorted_array = sort_array(test,len);
+  printf("\nSorted Array: ");
+  for(int i = 0;i<len;i++){
+    printf("%d,",sorted_array[i]);
+  }
+
+  median = find_median(sorted_array,len);
+  mean = find_mean(sorted_array,len);
+  min = find_minimum(sorted_array,len);
+  max = find_maximum(sorted_array,len);
+
+  print_statistics(min,max,mean,median);
+
   return(0);
 
 }
 
 
 void print_array(unsigned char array[],unsigned int length){
-
+  for(int i = 0;i<length;i++){
+    printf("%d,",array[i]);
+  }
 }
 
 unsigned char * sort_array(unsigned char array[],unsigned int length){
+  int value;
 
+  for(int i = 0;i<length;i++){
+    for(int j = i+1;j<length;j++){
+      if(array[i]>array[j]){
+        value = array[i];
+        array[i] = array[j];
+        array[j] = value;
+      }
+    }
+  }
+  /*for(int i = 0;i<length;i++){
+    printf("%d,",array[i]);
+  }*/
   return array;
 }
 
 void print_statistics(int min,int max,float mean,float median){
-
+  printf("\nMinimum: %d",min);
+  printf("\nMaximum: %d",max);
+  printf("\nMean: %f",mean);
+  printf("\nMedian: %f\n",median);
 }
 
 float find_median(unsigned char array[],unsigned int length){
-
+  float median;
+  if(length%2 == 0){
+    median = (array[length/2-1]+array[length/2])/2.0;
+  }
+  else{
+    median = array[length/2+1];
+  }
   return median;
 }
 
 float find_mean(unsigned char array[],unsigned int length){
-
+  double sum = 0;
+  
+  for(int i = 0;i<length;i++){
+    sum = sum + array[i];
+  }
+  sum = sum/length;
   return sum;
 }
 
 int find_minimum(unsigned char array[],unsigned int length){
-
+  int min;
+  min = array[0];
+  for(int i = 0;i<length;i++){
+    if(array[i]<min){
+      min = array[i];
+    }
+  }
   return min;
 }
 
 int find_maximum(unsigned char array[],unsigned int length){
-
+  int max;
+  max = array[0];
+  for(int i = 0;i<length;i++){
+    if(array[i]>max){
+      max = array[i];
+    }
+  }
   return max;
 }
 
